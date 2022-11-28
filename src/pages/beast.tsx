25b9489d24fecd631bc959bc68a93ad44135f702
lott-e <hi.lottie@icloud.com>
2022-11-28 23:18:28 +0100

    lint

--- src/pages/beast.tsx
+++ src/pages/beast.tsx
@@ -21,7 +21,7 @@ const Store: NextPageWithLayout = () => {
         <RefinementList refinementList={params} setRefinementList={setParams} />
         <InfiniteProducts params={params}  type="origin" />
       </div>
-      <h1>Peep's Merch</h1>
+      <h1>Peeps Merch</h1>
       <div className="flex flex-col small:flex-row small:items-start py-6">
  
         <RefinementList refinementList={params} setRefinementList={setParams} />
