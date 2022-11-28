e84fda032f2eb438ac7220565ed483d80e2dbfd7
lott-e <hi.lottie@icloud.com>
2022-11-28 23:20:33 +0100

    lint

--- src/modules/common/components/head/index.tsx
+++ src/modules/common/components/head/index.tsx
@@ -15,7 +15,7 @@ const Head: React.FC<HeadProps> = ({ title, description, image }) => {
       {description && <meta itemProp="description" content={description} />}
       {image && <meta itemProp="image" content={image} />}
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
       <link href="https://fonts.googleapis.com/css2?family=Bowlby+One&display=swap" rel="stylesheet"></link>
       <link rel="icon" href="/favicon.ico" />
       <link href="https://cdn.jsdelivr.net/npm/daisyui@2.42.1/dist/full.css" rel="stylesheet" type="text/css" />
